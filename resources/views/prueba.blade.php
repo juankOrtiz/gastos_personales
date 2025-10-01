92d9e35443e3dd3ab1e74fc2ee2e5536b5400bfd
juankOrtiz <juankortiz@proton.me>
2025-09-30 22:41:30 -0300

    Limpieza de archivos

--- resources/views/prueba.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('titulo', 'Pagina de prueba')
-
-@section('contenido')
-    <div class="container">
-        <h1>Esta es una pagina de prueba</h1>  
-    </div>
-@endsection
